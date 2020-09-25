6b358dc3930868d9bda158742c92e91c7ca0ad16
Matt Kasa <mkasa@gitlab.com>
2020-09-25 18:20:35 +0000

    Fix HTTP backend parameters for older versions of Terraform and add TF_STATE_NAME support

--- src/bin/gitlab-terraform.sh
+++ src/bin/gitlab-terraform.sh
@@ -22,9 +22,7 @@ JQ_PLAN='
 '
 
 # If TF_USERNAME is unset then default to GITLAB_USER_LOGIN
-if [ -z "${TF_USERNAME}" ]; then
-  TF_USERNAME="${GITLAB_USER_LOGIN}"
-fi
+TF_USERNAME="${TF_USERNAME:-${GITLAB_USER_LOGIN}}"
 
 # If TF_PASSWORD is unset then default to gitlab-ci-token/CI_JOB_TOKEN
 if [ -z "${TF_PASSWORD}" ]; then
@@ -32,17 +30,20 @@ if [ -z "${TF_PASSWORD}" ]; then
   TF_PASSWORD="${CI_JOB_TOKEN}"
 fi
 
-# Export variables for the HTTP backend if TF_ADDRESS is set
-if [ -n "${TF_ADDRESS}" ]; then
-  export TF_HTTP_ADDRESS="${TF_ADDRESS}"
-  export TF_HTTP_LOCK_ADDRESS="${TF_ADDRESS}/lock"
-  export TF_HTTP_LOCK_METHOD="POST"
-  export TF_HTTP_UNLOCK_ADDRESS="${TF_ADDRESS}/lock"
-  export TF_HTTP_UNLOCK_METHOD="DELETE"
-  export TF_HTTP_USERNAME="${TF_USERNAME}"
-  export TF_HTTP_PASSWORD="${TF_PASSWORD}"
-  export TF_HTTP_RETRY_WAIT_MIN="5"
+# If TF_ADDRESS is unset but TF_STATE_NAME is provided, then default to GitLab backend in current project
+if [ -n "${TF_STATE_NAME}" ]; then
+  TF_ADDRESS="${TF_ADDRESS:-${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/terraform/state/${TF_STATE_NAME}}"
 fi
+
+# Set variables for the HTTP backend to default to TF_* values
+export TF_HTTP_ADDRESS="${TF_HTTP_ADDRESS:-${TF_ADDRESS}}"
+export TF_HTTP_LOCK_ADDRESS="${TF_HTTP_LOCK_ADDRESS:-${TF_ADDRESS}/lock}"
+export TF_HTTP_LOCK_METHOD="${TF_HTTP_LOCK_METHOD:-POST}"
+export TF_HTTP_UNLOCK_ADDRESS="${TF_HTTP_UNLOCK_ADDRESS:-${TF_ADDRESS}/lock}"
+export TF_HTTP_UNLOCK_METHOD="${TF_HTTP_UNLOCK_METHOD:-DELETE}"
+export TF_HTTP_USERNAME="${TF_HTTP_USERNAME:-${TF_USERNAME}}"
+export TF_HTTP_PASSWORD="${TF_HTTP_PASSWORD:-${TF_PASSWORD}}"
+export TF_HTTP_RETRY_WAIT_MIN="${TF_HTTP_RETRY_WAIT_MIN:-5}"
 
 apply() {
   if ! terraform_is_at_least 0.13.2; then
@@ -52,7 +53,7 @@ apply() {
 }
 
 init() {
-  if [ -n "${TF_ADDRESS}" ] && ! terraform_is_at_least 0.13.2; then
+  if [ -n "${TF_HTTP_ADDRESS}" ] && ! terraform_is_at_least 0.13.2; then
     set -- \
       -backend-config=address="${TF_HTTP_ADDRESS}" \
       -backend-config=lock_address="${TF_HTTP_LOCK_ADDRESS}" \
