da0407141e28793f72c13ff4c30fb6f2b05a5ccc
Matt Kasa <mkasa@gitlab.com>
2021-01-28 02:32:57 +0000

    ci: Add local_file resource to test fixture

--- tests/main.tf
+++ tests/main.tf
@@ -1,3 +1,10 @@
 terraform {
   backend "http" { }
 }
+
+resource "local_file" "foo" {
+  filename = "${path.module}/foo.bar"
+  content = "bar!"
+  directory_permission = "0755"
+  file_permission = "0755"
+}
